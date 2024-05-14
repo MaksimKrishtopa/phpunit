ae2c49ed7ef129a07c8ee6ddd7aaa6c457121d42
Krishtopa Maksim <k.maks21@mail.ru>
2024-05-15 01:28:56 +0700

    Создание тестов по варианту

--- tests/CollectTest.php
+++ tests/CollectTest.php
@@ -4,10 +4,27 @@ use PHPUnit\Framework\TestCase;
 
 class CollectTest extends TestCase
 {
-    public function testCount()
+    public function testKeys()
     {
-        $collect = new Collect\Collect([13,17]);
-        $this->assertSame(2, $collect->count());
+        $collection = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
+        $this->assertEquals(['a', 'b', 'c'], $collection->keys()->toArray());
     }
 
+    public function testValues()
+    {
+        $collection = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
+        $this->assertEquals([1, 2, 3], $collection->values()->toArray());
+    }
+
+    public function testGet()
+    {
+        $collection = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
+        $this->assertEquals(2, $collection->get('b'));
+    }
+
+    public function testExcept()
+    {
+        $collection = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
+        $this->assertEquals(['a' => 1, 'c' => 3], $collection->except('b')->toArray());
+    }
 }
